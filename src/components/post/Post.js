82d12c46dd0b3ed75213f40ffe3ad31481502ff4
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-29 02:05:48 +0330

    add Link to Post component

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Post = ({post}) => {
-    return (
-        <li className="post">
-            <h2>{post.title}</h2>
-            <img src={post.img} alt="post" />
-            <div className="actions">
-               <i className={`fa ${post.isLiked ? 'fa-heart icon-red' : 'fa-heart-o'}`}></i>
-               <i className="fa fa-comment-o"></i>
-            </div>
-            <p>{post.description}</p> 
-        </li>
-    );
+const Post = ({ post }) => {
+  return (
+    <li className="post">
+      <h2>{post.title}</h2>
+      <img src={post.img} alt="post" />
+      <div className="actions">
+        <i className={`fa ${post.isLiked ? "fa-heart icon-red" : "fa-heart-o"}`}></i>
+        <Link to={`/single-post/${post.id}`}>
+          <i className="fa fa-comment-o"></i>
+        </Link>
+      </div>
+      <p>{post.description}</p>
+    </li>
+  );
 };
 
 export default Post;
