fb0d53de2e06782da6c0141cd8e796367a2b6700
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-29 00:15:39 +0330

    updata data

--- src/data.js
+++ src/data.js
@@ -19,6 +19,65 @@ const data = [
       },
     ],
   },
+  {
+    title: "Post2",
+    img: "./images/animal2.jpg",
+    description: "this is nice post 2",
+    isLiked: false,
+    comments: [
+      {
+        user: "ali 2",
+        msg: "nice post 2",
+      },
+    ],
+  },
+  {
+    title: "Post3",
+    img: "./images/animal3.jpg",
+    description: "this is nice post 3",
+    isLiked: false,
+    comments: [
+      {
+        user: "qolam 3",
+        msg: "nice post 3",
+      },
+    ],
+  },
+  {
+    title: "Post4",
+    img: "./images/animal4.jpg",
+    description: "this is nice post 4",
+    isLiked: true,
+    comments: [],
+  },
+  {
+    title: "Post5",
+    img: "./images/animal5.jpg",
+    description: "this is nice post 5",
+    isLiked: false,
+    comments: [
+      {
+        user: "ali",
+        msg: "nice post 5",
+      },
+      {
+        user: "mohammad",
+        msg: "good for you 5",
+      },
+    ],
+  },
+  {
+    title: "Post6",
+    img: "./images/animal6.jpg",
+    description: "this is nice post 6",
+    isLiked: true,
+    comments: [
+      {
+        user: "mina ",
+        msg: "nice post 6",
+      },
+    ],
+  },
 ];
 
 export default data;
