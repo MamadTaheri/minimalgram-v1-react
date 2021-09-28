5f6fb44c8611998bd12212d589aafb9d026db818
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-29 03:10:44 +0330

    update SinglePost

--- src/components/pages/SinglePost.js
+++ src/components/pages/SinglePost.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import Post from "../post/Post";
+
+import data from "../../data";
+
+import { useParams } from "react-router-dom";
+import Comments from "../comment/Comments";
 
 const SinglePost = () => {
-    return (
-        <div>
-            Single Post
-        </div>
-    );
+  const { id } = useParams();
+  return (
+    <div className="container">
+      <div className="single-post">
+        <Post post={data[id]} />
+        <Comments comments={data[id].comments}/>
+      </div>
+    </div>
+  );
 };
 
 export default SinglePost;
